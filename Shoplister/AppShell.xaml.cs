23e701d4dc4dd690e20610a9c39f0a286adc2189
Sullivan Lacheteau <lacheteau.s@gmail.com>
2022-10-08 00:01:10 -0400

    Views project folder + HomePage

--- Shoplister/AppShell.xaml.cs
+++ Shoplister/AppShell.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace Shoplister;
+﻿using Shoplister.Views;
+
+namespace Shoplister;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
 	}
 }
