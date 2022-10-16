7ba847a7c5b69927f667651a96f1981caa4e3430
Sullivan Lacheteau <lacheteau.s@gmail.com>
2022-10-15 23:04:11 -0400

    MDI Fonts for button icons

--- Shoplister/MauiProgram.cs
+++ Shoplister/MauiProgram.cs
@@ -17,6 +17,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("mdi-fonts.ttf", "mdi-fonts");
 			});
 
 		return builder.Build();
