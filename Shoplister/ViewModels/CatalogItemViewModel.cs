bd2d517e1c6b62e1b889629ae999918e6031e2e5
Sullivan Lacheteau <lacheteau.s@gmail.com>
2022-11-01 23:32:53 -0400

    Increment/Decrement quantity

--- Shoplister/ViewModels/CatalogItemViewModel.cs
+++ Shoplister/ViewModels/CatalogItemViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Shoplister.Models;
 
 namespace Shoplister.ViewModels;
@@ -7,14 +8,31 @@ public partial class CatalogItemViewModel : ObservableObject
 {
     private readonly Item _model;
 
+    [ObservableProperty]
+    private int _quantity;
+    
     public string Name => _model.Name;
-
-    public int Quantity => _model.Quantity;
 
     public DateTime CreationDate => _model.CreationDate;
 
     public CatalogItemViewModel(Item item)
     {
         _model = item;
+
+        Quantity = _model.Quantity;
+    }
+
+    [RelayCommand]
+    private void IncrementQuantity()
+    {
+        Quantity++;
+    }
+
+    [RelayCommand]
+    private void DecrementQuantity()
+    {
+        if (Quantity == 0) return;
+
+        Quantity--;
     }
 }
